a56bcd622bb13ab447ac6fac6d1058fd21079de8
afdalRafi3107 <afdalrafi990@gmail.com>
2025-02-09 11:42:43 +0700

    merubah tampilan testimanials degan boostrap card dan menggati navbar dnegan boostrap”

--- day1/personal-web/aset/js/testimonial.js
+++ day1/personal-web/aset/js/testimonial.js
@@ -1,43 +1,68 @@
-let testimonials = [
-    {
-        author:"afdal",
-        rating :5,
-        caption:"bagu banet",
-        Image:"my-iamge.jpg"
-    },
-    {
-        author:"rAFI",
-        rating :4,
-        caption:"Aku Suka",
-        Image:"my-iamge.jpg"
-    },
-    {
-        author:"asoka",
-        rating :3,
-        caption:"Luamyan Bagus",
-        Image:"my-iamge.jpg"
-    },
-];
-
-const testimonialsContainer = document.getElementById("testimonialsList");
-const testimonialsHTML = (daftarTestimoni) => {
-    return daftarTestimoni
-    .map(
+function fetchTestimonials() {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+  
+      xhr.open("GET", "https://api.npoint.io/37b03059009dda95802a", true);
+  
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          const response = JSON.parse(xhr.responseText);
+          // console.log("Response :", response);
+  
+          resolve(response.testimonials);
+        } else {
+          // console.error("Error :", xhr.status);
+          reject("Error :", xhr.status);
+        }
+      };
+      xhr.onerror = () => reject("network error");
+  
+      xhr.send();
+    });
+  }
+  
+  const testimonialsContainer = document.getElementById("testimonialsList");
+  
+  const testimonialsHTML = (array) => {
+    return array
+      .map(
         (testimonial) => `
-        <article>
-        <img src="aset/img/assets/img/coding.jpg" alt="">
-        <p class="testimonial-caption-item">
-          "keren buanget"
-        </p>
-        <p style="text-align: right;">- M Afdal</p>
-        <p style="text-align: right;">5★</p>
-      </article>
-        `
-    )
-    .join("")
-}
-function showAllTestimonials(){
+          <article>
+          <div class="card">
+          <img src="aset/img/assets/img/coding.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${testimonial.caption}</h5>
+            <p class="card-text" style="text-align: right;"> - ${testimonial.author}</p>
+            <p class="card-text" style="text-align: right;"> ${testimonial.rating}★</p>
+            
+          </div>
+        </div>
+          </article>
+          `
+      )
+      .join("");
+  };
+  
+  async function showAllTestimonials() {
+    const testimonials = await fetchTestimonials();
+    console.log(testimonials);
     testimonialsContainer.innerHTML = testimonialsHTML(testimonials);
-}
-
-showAllTestimonials()
+  }
+  
+  showAllTestimonials();
+  
+  async function filterTestimonialsByStar(rating) {
+    const testimonials = await fetchTestimonials();
+  
+    const filteredTestimonials = testimonials.filter(
+      (testimonial) => testimonial.rating === rating
+    );
+  
+    console.log(filteredTestimonials);
+  
+    if (filteredTestimonials.length === 0) {
+      return (testimonialsContainer.innerHTML = `<p>No testimonials.</p>`);
+    }
+  
+    testimonialsContainer.innerHTML = testimonialsHTML(filteredTestimonials);
+  }
